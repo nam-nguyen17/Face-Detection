4c6f15f511a01fae3f41914392a205d4e24a1112
Nam <ducnam1719@gmail.com>
2020-12-14 09:34:27 +0700

    face recognition

--- face_rec.py
+++ face_rec.py
@@ -45,8 +45,7 @@ def classify_face(img):
     faces_encoded = list(faces.values())
     known_face_names = list(faces.keys())
 
-    #img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
-    #img = img[:,:,::-1]
+   
  
     face_locations = face_recognition.face_locations(img)
     unknown_face_encodings = face_recognition.face_encodings(img, face_locations)
@@ -66,22 +65,5 @@ def classify_face(img):
 
         face_names.append(name)
 
-        # for (top, right, bottom, left), name in zip(face_locations, face_names):
-        #     # Draw a box around the face
-        #     cv2.rectangle(img, (left-20, top-20), (right+20, bottom+20), (255, 0, 0), 2)
-
-        #     # Draw a label with a name below the face
-        #     cv2.rectangle(img, (left-20, bottom -15), (right+20, bottom+20), (255, 0, 0), cv2.FILLED)
-        #     font = cv2.FONT_HERSHEY_DUPLEX
-        #     cv2.putText(img, name, (left -20, bottom + 15), font, 1.0, (255, 255, 255), 2)
-
-
-    # Display the resulting image
-    # while True:
-
-    #     cv2.imshow('Video', img)
-    #     if cv2.waitKey(1) & 0xFF == ord('q'):
+    
     return face_names 
-
-
-# print(classify_face("IMG-13-12-2020-20-35-48.jpg"))
